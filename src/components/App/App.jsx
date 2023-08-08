993d615dd3e3beda5d2977eedd8c402e281debe8
yuliahalaniuk <122388855+yuliahalaniuk@users.noreply.github.com>
2023-08-08 19:10:26 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,17 +2,6 @@ import React, { Component } from 'react';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+
   );
 };
